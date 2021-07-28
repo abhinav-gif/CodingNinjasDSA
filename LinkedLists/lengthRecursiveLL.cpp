11fa7c22683fbb3caf8932f0e6d4a179acd5bfaf
Abhinav <17122000abhinav@gmail.com>
2021-07-28 23:36:23 +0530

    commit

---
+++ LinkedLists/lengthRecursiveLL.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+int len(Node *head)
+{
+    if (head == NULL)
+    {
+        return 0;
+    }
+    return 1 + len(head->next);
+}
+
+Node *input()
+{
+    int data;
+    cin >> data;
+    Node *head = NULL;
+    Node *tail = NULL;
+    while (data != -1)
+    {
+        Node *n = new Node(data);
+        if (head == NULL)
+        {
+            head = n;
+            tail = n;
+        }
+        else
+        {
+            tail->next = n;
+            tail = tail->next;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = input();
+    cout << len(head) << endl;
+}
