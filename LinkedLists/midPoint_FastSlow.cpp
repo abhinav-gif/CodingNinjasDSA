f2640c3044a341e6ef116ae0bf6e888729c3e085
Abhinav <17122000abhinav@gmail.com>
2021-07-31 21:36:10 +0530

    commit

---
+++ LinkedLists/midPoint_FastSlow.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+Node *takeinput()
+{
+    int data;
+    cin >> data;
+    Node *head = NULL;
+    Node *tail = NULL;
+    while (data != -1)
+    {
+        Node *n = new Node(data);
+        if (head == NULL)
+        {
+            head = n;
+            tail = n;
+        }
+        else
+        {
+            tail->next = n;
+            tail = tail->next;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+int mid(Node *head)
+{
+    Node *slow = head;
+    Node *fast = head;
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow->data;
+}
+
+int main()
+{
+    Node *head = takeinput();
+    cout << mid(head) << endl;
+}
